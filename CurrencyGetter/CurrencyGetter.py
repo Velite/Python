05085935cb0d923d6724dd84eed27ae97661c342
Velite <Velite@users.noreply.github.com>
2015-08-25 11:34:48 +0300

    Update CurrencyGetter.py

--- CurrencyGetter/CurrencyGetter.py
+++ CurrencyGetter/CurrencyGetter.py
@@ -1,10 +1,24 @@
-from datetime import date
 from urllib.request import urlopen
+import datetime, psycopg2
 
 
-url = "http://cbrates.rbc.ru/tsv/"
-currencyCode = 840
-resurl = url + str(currencyCode) + "/" + date.today().strftime("%Y/%m/%d") + ".tsv"
-response = urlopen(resurl)
-content = response.read()
-print(content)
+def get_value(currencyCode):
+	url = "http://cbrates.rbc.ru/tsv/"
+	date = datetime.datetime.now()
+	requestUrl = url + str(currencyCode) + "/" + date.strftime("%Y/%m/%d") + ".tsv"
+	response = urlopen(requestUrl)
+	content = response.read()
+	content = str(content).replace("1\t", "")
+	value = float(content[5:-3])
+	insert_to_db(value, currencyCode, date)
+
+def insert_to_db(value, currencyCode, date):
+	conn = psycopg2.connect("host='***' port=*** user='***' password='***' dbname='***'")
+	cursor = conn.cursor()
+	cursor.execute("""INSERT INTO currency_table("Value", "Code", "Date") VALUES (%s, %s, %s);""", (value, currencyCode, date))
+	conn.commit()
+	cursor.close()
+	conn.close()
+
+get_value(840)
+get_value(978)
